6183a1cbe33ad19029138ffb6dba720cd01fd4ce
Harshavardhan <devarakondaharshavardhan187@gmail.com>
2025-02-14 15:52:56 +0530

    new

---
+++ python/Day11/classwork17.py
@@ -0,0 +1,23 @@
+#print("*******************")
+ 
+def make_pretty(func):
+ 
+    def inner():
+
+        print("********************")
+
+        func()
+
+        print("********************")
+
+    return inner
+ 
+@make_pretty
+
+def ordinary():
+
+    print("I am ordinary")
+ 
+ordinary()  
+
+ 
