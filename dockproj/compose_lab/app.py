21c494ba685b48d165599c04f3ad8341f14692f2
Harshavardhan <devarakondaharshavardhan187@gmail.com>
2025-03-03 20:34:48 +0530

    file

---
+++ dockproj/compose_lab/app.py
@@ -0,0 +1,10 @@
+#assignment1
+dict_loc = {
+    'harsha': 'tirupati',
+    'hari': 'kadapa',
+    'muni': 'durgasamudram'
+}
+print("Keys:", list(dict_loc.keys()))
+print("Values:", list(dict_loc.values()))
+for k, v in dict_loc.items():
+    print(f"{k} ---> {v}")
